d4166ad08ac33dd650e9509ee25ce6057a67b0e1
Pavol Hejny <me@pavolhejny.com>
2022-04-03 21:57:02 +0200

    Importing multiple pages of PDF

--- src/pdf-import.module.ts
+++ src/pdf-import.module.ts
@@ -6,6 +6,8 @@ import {
     fitInside,
     ImageArt,
     measureImageSize,
+    ShapeArt,
+    ShapeName,
 } from '@collboard/modules-sdk';
 import { Registration } from 'destroyable';
 import { Vector } from 'xyzt';
@@ -38,18 +40,19 @@ declareModule({
 
         return importSystem.registerFileSupport({
             priority: 10,
-            async processFile({ logger, file, boardPosition, previewOperation, next }) {
+            async processFile({ logger, file, boardPosition, previewOperation, willCommitArts, next }) {
                 if (file.type !== 'application/pdf') {
                     return next();
                 }
 
-                // Note: DO NOT select created arts - DO not call willCommitArts
+                willCommitArts();
 
                 const pdfFile = file;
 
                 const pdfDataUrl = await blobToDataUrl(pdfFile);
                 const imagesDataUrl = await pdfToImages(pdfDataUrl);
 
+                // Note: DO NOT select created arts by not returning operation
                 const result = Registration.void();
 
                 for (const [i, imageDataUrl] of imagesDataUrl.entries()) {
@@ -65,24 +68,37 @@ declareModule({
                     imageArt.opacity = 0.5;
                     imageArt.locked = true;
 
+                    const borderArt = new ShapeArt(
+                        ShapeName.Rectange,
+                        '#ccc',
+                        3 / appState.transform.scale.x,
+                        imageArt.shift,
+                        imageArt.size,
+                    );
+                    // TODO: Maybe borderArt.opacity= 0.9;
+                    borderArt.locked = true;
+
                     centerArts({
-                        arts: [imageArt],
+                        arts: [imageArt, borderArt],
                         boardPosition,
                     });
 
                     logger.info(`Page ${i + 1}/${imagesDataUrl.length}`, imageArt);
 
-                    boardPosition = boardPosition.add(
-                        new Vector(imageArt.size).rearrangeAxis(([x, y, z]) => [0, y, 0]),
-                    );
+                    boardPosition = boardPosition
+                        .add(new Vector(imageArt.size).rearrangeAxis(([x, y, z]) => [0, y, 0]))
+                        .add(new Vector(0, 30).scale(1 / appState.transform.scale.x));
 
-                    previewOperation.update(imageArt);
+                    previewOperation.update(imageArt /* TODO: Also borderArt */);
 
                     const imageSrc = await apiClient.fileUpload(await dataUrlToBlob(imageDataUrl));
                     imageArt.src = imageSrc;
                     imageArt.opacity = 1;
 
-                    const operation = materialArtVersioningSystem.createPrimaryOperation().newArts(imageArt).persist();
+                    const operation = materialArtVersioningSystem
+                        .createPrimaryOperation()
+                        .newArts(imageArt, borderArt)
+                        .persist();
                     result.addSubdestroyable(operation);
                 }
 
@@ -93,7 +109,7 @@ declareModule({
 });
 
 /**
- * TODO: !!! Add some frame
+ * TODO: Do not make border by ShapeArt but make some better ImageArt
  * TODO: Better tooling around PDFs
  * TODO: Maybe create PdfArt
  */
