0befd193b8197fe7a1e88e3caf67bbe00d2a9b13
Pavol Hejny <me@pavolhejny.com>
2022-04-03 21:37:57 +0200

    Importing multiple pages

--- src/pdf-import.module.ts
+++ src/pdf-import.module.ts
@@ -7,6 +7,8 @@ import {
     ImageArt,
     measureImageSize,
 } from '@collboard/modules-sdk';
+import { Registration } from 'destroyable';
+import { Vector } from 'xyzt';
 import { contributors, description, license, repository, version } from '../package.json';
 import { pdfToImages } from './pdfToImages';
 
@@ -46,39 +48,52 @@ declareModule({
                 const pdfFile = file;
 
                 const pdfDataUrl = await blobToDataUrl(pdfFile);
-                const imageDataUrl = await pdfToImages(pdfDataUrl);
+                const imagesDataUrl = await pdfToImages(pdfDataUrl);
 
-                // await previewImage(imageSrc);
+                const result = Registration.void();
 
-                const imageScaledSize = fitInside({
-                    isUpscaling: false,
-                    objectSize: await (await measureImageSize(imageDataUrl)).divide(appState.transform.scale),
-                    containerSize: appState.windowSize.divide(appState.transform.scale),
-                });
+                for (const [i, imageDataUrl] of imagesDataUrl.entries()) {
+                    const imageArt = new ImageArt(
+                        imageDataUrl,
+                        `Page ${i + 1}/${imagesDataUrl.length} of ${pdfFile.name}`,
+                    );
+                    imageArt.size = fitInside({
+                        isUpscaling: false,
+                        objectSize: await (await measureImageSize(imageDataUrl)).divide(appState.transform.scale),
+                        containerSize: appState.windowSize.divide(appState.transform.scale),
+                    });
+                    imageArt.opacity = 0.5;
+                    imageArt.locked = true;
 
-                const imageArt = new ImageArt(imageDataUrl, 'image');
-                imageArt.size = imageScaledSize;
-                imageArt.opacity = 0.5;
+                    centerArts({
+                        arts: [imageArt],
+                        boardPosition,
+                    });
 
-                logger.info('Imported art', imageArt);
+                    logger.info(`Page ${i + 1}/${imagesDataUrl.length}`, imageArt);
 
-                centerArts({ arts: [imageArt], boardPosition });
+                    boardPosition = boardPosition.add(
+                        new Vector(imageArt.size).rearrangeAxis(([x, y, z]) => [0, y, 0]),
+                    );
 
-                previewOperation.update(imageArt);
+                    previewOperation.update(imageArt);
 
-                const imageSrc = await apiClient.fileUpload(await dataUrlToBlob(imageDataUrl));
-                imageArt.src = imageSrc;
-                imageArt.opacity = 1;
+                    const imageSrc = await apiClient.fileUpload(await dataUrlToBlob(imageDataUrl));
+                    imageArt.src = imageSrc;
+                    imageArt.opacity = 1;
 
-                const operation = materialArtVersioningSystem.createPrimaryOperation().newArts(imageArt).persist();
+                    const operation = materialArtVersioningSystem.createPrimaryOperation().newArts(imageArt).persist();
+                    result.addSubdestroyable(operation);
+                }
 
-                return operation;
+                return result;
             },
         });
     },
 });
 
 /**
+ * TODO: !!! Add some frame
  * TODO: Better tooling around PDFs
  * TODO: Maybe create PdfArt
  */
